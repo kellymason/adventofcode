4dd0021b02a2c6d5111dd1940a838caba5059453
Kelly Mason <kellymason@github.com>
2021-12-09 11:01:22 -0700

    day 8, mess around with 2020 day 10

--- 2020/day-10/part_2.rb
+++ 2020/day-10/part_2.rb
@@ -1,41 +1,88 @@
-# CORRECT_ANSWER = 2450
-TEST_INPUT = [16, 10, 15, 5, 1, 11, 7, 19, 6, 12, 4]
+class DayTenPartTwo
+  def initialize(test: true)
+    input_file = test ? "test_input.txt" : "input.txt"
+    @input = IO.readlines(input_file).map{|l| l.to_i}.sort
+    # puts "input: #{@input.inspect}"
+  end
 
-def get_input
-  IO.readlines("input.txt").map{|l| l.to_i}.sort!
-end
+  # takes in input as a sorted array of ints
+  # maps the list of valid next steps for each adapter
+  # goes through each value of each next step
+  # def count_combos
+  #   combo_count = 0
+  #   current_range = []
+  #   @input.each_with_index do |joltage, i|
+  #     current_combo_count = 0
+  #     if i > 0 && joltage - @input[i-1] == 3
+  #       # puts "#{joltage} - #{@input[i-1]} is 3"
+  #       # count the combos of current range,
+  #       # increment the combo_count, 
+  #       if current_range.length > 1
+  #         (1).upto(current_range.length) do |i|
+  #           puts "combos to add: #{current_range.combination(i).to_a}"
+  #           current_combo_count += (current_range.combination(i).to_a.count - 1)
+  #           combo_count += current_combo_count
+  #         end
+  #       end
+  #       # then clear the current range
+  #       current_range = []
+  #     else
+  #       current_range.push(joltage)
+  #       puts "current_range: #{current_range}"
+  #     end
+  #   end
+  #   return combo_count
+  # end
 
-# takes in input as a sorted array of ints
-# maps the list of valid next steps for each adapter
-# goes through each value of each next step
-def count_combos(input)
-  combo_count = 0
-  current_range = []
-  input.each_with_index do |joltage, i|
-    if i > 0 && joltage - input[i-1] == 3
-      # puts "#{joltage} - #{input[i-1]} is 3"
-      # count the combos of current range,
-      # increment the combo_count, 
-      if current_range.length > 1
-        (1).upto(current_range.length) do |i|
-          puts "combos to add: #{current_range.combination(i).to_a}"
-          current_combo_count += (current_range.combination(i).to_a.count - 1)
-          combo_count += current_combo_count
-        end
+  # add zero to the beginning of the array to represent the outlet
+  # Change the data to look something like this:
+  # {
+  #   0: [1],
+  #   1: [4],
+  #   4: [5,6,7],
+  #   5: [6,7],
+  # }
+
+    # iterate through the sorted joltages
+    # for each joltage value, save a list of valid next adapters
+    # 
+
+  def fill_in_hash
+    @joltages_hash = @input.unshift(0).to_h { |k| [k, nil] }
+    puts @joltages_hash.inspect
+    # for each joltage in our input list,
+    # set the joltage as a key and an array of the possible next values as the value
+    adapter_count = @input.length
+    @input.each_with_index do |joltage, i|
+      if i == adapter_count - 1
+        # last adapter
+        @joltages_hash.delete(joltage)
+        return @joltages_hash
+      elsif i == adapter_count - 2
+        # second to last adapter, only one step from here
+        possible_next_joltages = [@input[i + 1]]
+      elsif i == adapter_count - 3
+        possible_next_joltages = [@input[i+1], @input[i+2]].select{ |higher_joltage| (higher_joltage - joltage) <= 3 }
+      else
+        possible_next_joltages =[@input[i+1],@input[i+2],@input[i+3]].select{ |higher_joltage| (higher_joltage - joltage) <= 3 }
+        # puts "possible next adapters: #{possible_next_joltages}"
       end
-      # then clear the current range
-      current_range = []
-    else
-      current_range.push(joltage)
-      puts "current_range: #{current_range}"
+      @joltages_hash[joltage] = possible_next_joltages
     end
   end
-  return combo_count
+
+# todo: count number of values each key has, do a factorial
+  def count_combos
+    puts @joltages_hash.values.inspect
+    choice_points = @joltages_hash.values.filter{ |val| val.length > 1 }
+    # array of values greater than 1 representing how many choices there are for the adapter
+    choice_points.sum{ |choice_count| (1..choice_count).inject(:*) }
+  end
+
+  def get_final_answer
+    fill_in_hash
+    puts count_combos
+  end
 end
 
-def get_final_answer
-  input = TEST_INPUT.sort
-  count_combos(input)
-end
-
-puts get_final_answer
+puts DayTenPartTwo.new().get_final_answer
--- 2021/day_8/day_8.rb
+++ 2021/day_8/day_8.rb
@@ -1,8 +1,116 @@
 class DayEight
   def initialize(test: true)
     input_file = test ? "test_input.txt" : "input.txt"
-    @input = File.readlines(input_file, chomp: true)
+    input = File.readlines(input_file, chomp: true)
+    # an array consisting of nested arrays representing 4-digit output values
+    # "be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | dgacbe cefdb cefbgd gcbe"
+    # [
+      #   ["fdgacbe", "cefdb", "cefbgd", gcbe"],
+      #   ["fdgacbe", "cefdb", "cefbgd", gcbe"],
+      #   ["fdgacbe", "cefdb", "cefbgd", gcbe"],
+      # ]
+    @output_values = input.map{ |l| l.split("|").last.split(" ") }
+    @signal_patterns = input.map{ |l| l.split("|").first.split(" ") }
+  end
+
+  # ones require 2 segments
+  # fours require 4 segments
+  # sevens require 3 segments
+  # eights require 7 segments
+  # go through @output_values, increment a counter if length is equal to one of these
+  def count_ones_fours_sevens_and_eights
+    counter = 0
+    @output_values.flatten.each{ |value| counter += 1 if [2,3,4,7].include?(value.length) }
+    puts counter
+  end
+
+  # determine which letter corresponds to which line segment
+  # represent the line segments as positions 0 - 6, where 0 is top, moves clockwise, 6 is middle
+  # find the segment letters that comprise the easy display_numbers (one, four, seven, eight)
+  # all segments: 8
+  # all segments - 1: 0,6,9
+  # all segemnts - 2: 2,3,5
+  # four segments: 4
+  # three segments: 7
+  # two segments: 1
+  def key_with_one_four_seven_eight(line)
+    key = Hash[(0..9).map { |num| [num, nil] }]
+    remaining_nums = line.dup
+    line.each do |pattern|
+      letters = pattern.split("").uniq.sort
+      case letters.length
+      when 2
+        key[1] = letters
+        remaining_nums.delete(pattern)
+      when 3
+        key[7] = letters
+        remaining_nums.delete(pattern)
+      when 4
+        key[4] = letters
+        remaining_nums.delete(pattern)
+      when 7
+        key[8] = letters
+        remaining_nums.delete(pattern)
+      end
+    end
+    [remaining_nums, key]
+  end
+
+  # using what we have in the @key, try and figure out the other numbers
+
+  def determine_other_numbers(line, key)
+    line.each do |pattern|
+      letters = pattern.split("").sort
+      case letters.length
+      when 5
+        # possibilities: 2,3,5
+        if key[1].all?{ |one_segment| letters.include?(one_segment) }
+          key[3] = letters
+        elsif (letters & key[1]).length == 1 && (letters & key[4]).length == 2
+          key[2] = letters
+        else 
+          key[5] = letters
+        end
+        #  else its a 2
+      when 6
+        # possibilities: 0,6,9
+        # if the letters don't contain the letters for 1, it's a 6
+        # if the letters dont contain the letters for 4, it's a 0
+        # else, 9
+        if key[1] && !key[1].all?{ |one_segment| letters.include?(one_segment) }
+          key[6] = letters
+        elsif key[4] && !key[4].all?{ |four_segment| letters.include?(four_segment) }
+          key[0] = letters
+        else
+          key[9] = letters
+        end
+      end
+    end
+    key
+  end
+
+  # given a key of segment mappings
+  # apply to four-digit output and use to return the 4-digit integer representing the display number
+  def get_display_values_for_line(line, key)
+    # array representing four integer digits
+    display_value = []
+    line.each do |str|
+      sorted_value_for_hash_comparison = str.split("").sort
+      display_value.push(key.key(sorted_value_for_hash_comparison))
+    end
+    display_value.join.to_i
+  end
+
+  def sum_of_output_values
+    numbers_to_sum = []
+    @signal_patterns.each_with_index do |pattern_array, i|
+      remaining_nums, key = key_with_one_four_seven_eight(pattern_array)
+      determine_other_numbers(remaining_nums, key)
+      numbers_to_sum.push(get_display_values_for_line(@output_values[i], key))
+    end
+    puts numbers_to_sum.sum
   end
 end
 
-DayEight.new()
+# DayEight.new().count_ones_fours_sevens_and_eights
+DayEight.new(test: false).sum_of_output_values
